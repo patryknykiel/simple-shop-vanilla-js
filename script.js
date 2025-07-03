245fb27afe7eb62efa69997b09d406bdd00715a3
Patryk Nykiel <pnykiel@sii.pl>
2025-07-03 13:22:01 +0200

    Dodano dodawanie i usuwanie produktow z koszyka

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 document.querySelectorAll(".add-to-basket").forEach((b) => b.addEventListener('click', (e) => {
-    const deleteFromBasketButton = document.createElement("button");
-    deleteFromBasketButton.innerHTML = "Delete";
-    deleteFromBasketButton.classList.add("delete-from-basket");
-    const productName = e.target.parentElement.querySelector("p").innerHTML;
+    
+    const productName = e.target.parentElement.querySelector(".product-name").innerHTML;
+    const manufacturerName = e.target.parentElement.querySelector(".manufacturer-name").innerHTML;
+    console.log(manufacturerName);
+    console.log(productName);
     const productImg = e.target.parentElement.querySelector("img").src;
+    
     const newDiv = document.createElement("div");
-    const p = document.createElement("p");
-    p.innerHTML = productName;
+    const pProductName = document.createElement("p");
+    const pManufacturerName = document.createElement("p");
     const img = document.createElement("img");
+    const deleteFromBasketButton = document.createElement("button");
+
+    pProductName.innerHTML = productName;
+    pManufacturerName.innerHTML = manufacturerName;
     img.src = productImg;
+    deleteFromBasketButton.value = "Delete";
+
     newDiv.classList.add("product-in-basket");
     document.querySelector(".basket-list").appendChild(newDiv);
+    deleteFromBasketButton.classList.add("delete-from-basket");
+
     newDiv.appendChild(img);
-    newDiv.appendChild(p);
+    newDiv.appendChild(pProductName);
+    newDiv.appendChild(pManufacturerName);
     newDiv.appendChild(deleteFromBasketButton);
 
     document.querySelectorAll(".delete-from-basket").forEach((b) => b.addEventListener('click', (e) => {
-    e.target.parentElement.remove();    
-}));
+        e.target.parentElement.remove();
+    }));
+    
 
 }));
+
+
