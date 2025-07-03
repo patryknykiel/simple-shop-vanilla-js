408ee30f60e279c117e781ef2704e7b58166e2e0
Patryk Nykiel <pnykiel@sii.pl>
2025-07-03 14:26:25 +0200

    Save point

--- script.js
+++ script.js
@@ -4,26 +4,26 @@ document.querySelectorAll(".add-to-basket").forEach((b) => b.addEventListener('c
     const manufacturerName = e.target.parentElement.querySelector(".manufacturer-name").innerHTML;
     console.log(manufacturerName);
     console.log(productName);
-    const productImg = e.target.parentElement.querySelector("img").src;
     
     const newDiv = document.createElement("div");
     const pProductName = document.createElement("p");
     const pManufacturerName = document.createElement("p");
-    const img = document.createElement("img");
     const deleteFromBasketButton = document.createElement("button");
+    const productCheckbox =  document.createElement("input");
 
     pProductName.innerHTML = productName;
     pManufacturerName.innerHTML = manufacturerName;
-    img.src = productImg;
-    deleteFromBasketButton.value = "Delete";
+    deleteFromBasketButton.innerText = "Delete";
+    productCheckbox.type = "checkbox"
 
     newDiv.classList.add("product-in-basket");
+    pManufacturerName.classList.add("manufacturer-name-basket");
     document.querySelector(".basket-list").appendChild(newDiv);
     deleteFromBasketButton.classList.add("delete-from-basket");
 
-    newDiv.appendChild(img);
-    newDiv.appendChild(pProductName);
+    newDiv.appendChild(productCheckbox);
     newDiv.appendChild(pManufacturerName);
+    newDiv.appendChild(pProductName);
     newDiv.appendChild(deleteFromBasketButton);
 
     document.querySelectorAll(".delete-from-basket").forEach((b) => b.addEventListener('click', (e) => {
