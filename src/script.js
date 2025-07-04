32541d1de5a0713834922468ad7594c453b39b20
Patryk Nykiel <pnykiel@sii.pl>
2025-07-04 11:06:47 +0200

    Dodano pobieranie produktow z JSON ale przestalo dziaalc dodowanie i usuwanie z koszyka trzeba poprawic ale teraz nie trzeba scrapowac tylko mozna poslugiwac sie json

--- src/script.js
+++ src/script.js
@@ -1,14 +1,70 @@
+fetch('../products.json')
+  .then(response => response.json())
+  .then(data => {
+    addProductsToShop(data); 
+  })
+  .catch(error => {
+    console.error('Error loading the JSON file:', error);
+  });
+
+
+
+function addProductsToShop(data) {
+
+    Object.entries(data.products).forEach((product) => {
+
+        const newDiv = document.createElement("div");
+        const productImg = document.createElement("img");
+        const productName = document.createElement("p");
+        const manufacturerName = document.createElement("p");
+        const description = document.createElement("p");
+        const productPrice = document.createElement("p");
+        const quantityInput = document.createElement("input");
+        const changeQuantityDiv = document.createElement("div");
+        const changeQuantityPlus = document.createElement("button");
+        const changeQuantityMinus = document.createElement("button");
+        const addToBasket = document.createElement("button");
+
+
+        productName.innerText = product[0];
+        manufacturerName.innerText = product[1].manufacturer;
+        productImg.src = product[1].img;
+        description.innerText = product[1].description;
+        productPrice.innerText = product[1].price;     
+
+
+        newDiv.classList.add("item");
+        productName.classList.add("product-name");
+        manufacturerName.classList.add("manufacturer-name");
+        description.classList.add("description");
+        productPrice.classList.add("product-price");
+        quantityInput.type = "text";
+        changeQuantityPlus.innerText = "+";
+        changeQuantityMinus.innerText = "-";
+        addToBasket.innerText = "Add to basket";
+        addToBasket.classList.add("add-to-basket");
+
+
+        document.querySelector(".items-list").appendChild(newDiv);
+        newDiv.appendChild(productImg);
+        newDiv.appendChild(productName);
+        newDiv.appendChild(manufacturerName);
+        newDiv.appendChild(description);
+        newDiv.appendChild(productPrice);
+        newDiv.appendChild(quantityInput);
+        newDiv.appendChild(changeQuantityDiv);
+        changeQuantityDiv.appendChild(changeQuantityPlus);
+        changeQuantityDiv.appendChild(changeQuantityMinus);
+        newDiv.appendChild(addToBasket);
+
+    });
+};
+
+
 document.querySelectorAll(".add-to-basket").forEach((b) => b.addEventListener('click', (e) => {
     
     const productName = e.target.parentElement.querySelector(".product-name").innerText;
     const manufacturerName = e.target.parentElement.querySelector(".manufacturer-name").innerText;
-
-
-   document.querySelectorAll(".basket-list").forEach((div) => {
-    console.log(div.children.querySelector("."));
-   })
-
-
     const productPrice = e.target.parentElement.querySelector(".product-price").innerText;
     
     const newDiv = document.createElement("div");
@@ -54,10 +110,19 @@ document.querySelectorAll(".add-to-basket").forEach((b) => b.addEventListener('c
     newDiv.appendChild(deleteFromBasketButton);
     newDiv.appendChild(total);
 
+    document.querySelectorAll(".manufacturer-name-basket").forEach((name) => {
+
+
+    manufacturerInBasket.push(name.innerText);
+    console.log(manufacturerInBasket);
+   })
+
     document.querySelectorAll(".delete-from-basket").forEach((b) => b.addEventListener('click', (e) => {
         e.target.parentElement.remove();
     }));
     
+
+    manufacturerInBasket = [];
 
 }));
 
